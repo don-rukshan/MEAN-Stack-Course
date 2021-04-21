393723672e59273d79210a1e6d49be3d439daae9
Don Rukshan <md.rukshan@gmail.com>
2021-04-21 22:39:07 +0530

    connecting mongoDB done

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://rukshan:Lb7moRgHTGKrcfLy@cluster0.ubqeb.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to database"))
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,31 +31,23 @@ app.use((req, res, next) => {
   );
   next();
 });
-
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({
     message: "Post added successfully",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: uuidv4(),
-      title: "first server-side post",
-      content: "this is coming from the server",
-    },
-    {
-      id: uuidv4(),
-      title: "second server-side post",
-      content: "this is coming from the server!!!",
-    },
-  ];
-  res.status(200).json({
-    message: " Posts fetched successfully! ",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: " Posts fetched successfully! ",
+      posts: documents,
+    });
   });
 });
 
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -9,12 +9,6 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  // posts = [
-  //   { title: 'First Post', content: "This is th first post's content" },
-  //   { title: 'Second Post', content: "This is th Second post's content" },
-  //   { title: 'Third Post', content: "This is th Third post's content" },
-  // ];
-
   posts: Post[] = [];
   private postsSub: Subscription;
 
