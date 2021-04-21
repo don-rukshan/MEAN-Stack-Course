0e362e63029fcbc3669a839914a75bc4df396988
Don Rukshan <md.rukshan@gmail.com>
2021-04-21 17:08:59 +0530

    added express

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const app = require("./backend/app");
+
+const PORT = process.env.PORT || 3000;
+
+app.set("port", PORT);
+const server = http.createServer(app);
+
+server.listen(PORT);
