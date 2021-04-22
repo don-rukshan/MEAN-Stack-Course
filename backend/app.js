e5763d300575ecefa5cb46deb484b1c729cf581d
Don Rukshan <md.rukshan@gmail.com>
2021-04-22 12:19:22 +0530

    router and edit and spinner done

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 
+const postsRoutes = require("./routes/posts");
+
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
 const app = express();
 
 mongoose
@@ -27,37 +28,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,POST,PATCH,DELETE,OPTIONS"
+    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
   );
   next();
 });
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((result) => {
-    res.status(201).json({
-      message: "Post added successfully",
-      postId: result._id,
-    });
-  });
-});
 
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: " Posts fetched successfully! ",
-      posts: documents,
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
+app.use("api/posts", postsRoutes);
 
 module.exports = app;
